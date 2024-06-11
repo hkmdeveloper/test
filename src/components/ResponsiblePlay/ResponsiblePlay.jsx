bb5b34d1e3846fb5e9f3332ce7fff94e2a048e28
hkmdeveloper <developer@hkmedia.co.in>
2024-06-11 08:33:53 +0530

    Update ResponsiblePlay.jsx

--- src/components/ResponsiblePlay/ResponsiblePlay.jsx
+++ src/components/ResponsiblePlay/ResponsiblePlay.jsx
@@ -3,28 +3,28 @@ import "./ResponsiblePlay.css";
 
 export const ResponsiblePlay = () => {
   return (
-    <div className="privacy-policy-container">
+    <div className="responsible-play-container">
 
-<p className="policy-para">......</p>
-      <div className="each-policy-container">
-        <h3 className="policy-heading">Responsible Play</h3>
-        <p className="policy-para">
+<p className="responsible-para">......</p>
+      <div className="each-responsible-container">
+        <h3 className="responsible-heading">Responsible Play</h3>
+        <p className="responsible-para">
         What is MyWicket11?
         </p>
 
-        <p className="policy-para">MyWicket11 is one of the leading online fantasy sport game to play online cricket
+        <p className="responsible-para">MyWicket11 is one of the leading online fantasy sport game to play online cricket
          leagues. Users can make their own team according to their sports knowledge and win points.</p>
 
-        <p className="policy-para">How do I sign up?</p>
+        <p className="responsible-para">How do I sign up?</p>
 
 
-        <p className="policy-para">The users can sign up on MyWicket11 by filling a short registration 
+        <p className="responsible-para">The users can sign up on MyWicket11 by filling a short registration 
         form available on the homepage of MyWicket11 website. However, the users can also signup directly
          by using their Facebook or Google+ account.</p>
 
-        <p className="policy-para">Why can’t I log in to my account?</p>
+        <p className="responsible-para">Why can’t I log in to my account?</p>
 
-        <p className="policy-para">The users may encounter signup problems due to various reasons. 
+        <p className="responsible-para">The users may encounter signup problems due to various reasons. 
         The primary reasons due to which the user might not be able to signup can include the user might
          be entering incorrect email address or password. In case the user has forgotten his/her password, 
          there’s a Forgot Password link available below the login option. By clicking on the Forgot Password link,
@@ -41,45 +41,45 @@ export const ResponsiblePlay = () => {
 
 
 
-      <div className="each-policy-container">
-        <p className="policy-para">
+      <div className="each-responsible-container">
+        <p className="responsible-para">
         Can I have multiple accounts on MyWicket11?
         </p>
        
-        <p className="policy-para">
+        <p className="responsible-para">
         No. A user is allowed to have only one account under its name with valid supporting documents 
-        on MyWicket11. Please refer Fair Play policy for further details.
+        on MyWicket11. Please refer Fair Play responsible for further details.
         </p>
-        <p className="policy-para">
+        <p className="responsible-para">
         Can I update/edit my information?
         </p>
 
-        <p className="policy-para">The users can easily update/edit their information by logging into their MyWicket11
+        <p className="responsible-para">The users can easily update/edit their information by logging into their MyWicket11
          accounts using their registered email address and password. After logging into their MyWicket11 account the 
          users can edit/update their information by clicking on ‘Edit Profile’ on the top right of the page. The users 
          will be able to edit their basic information including date of birth, profile name and address but the users 
          won’t be allowed to change their email address, mobile number or team name once registered with MyWicket11.</p>
-        <p className="policy-para">I did not get any confirmation email after I signed up.</p>
-        <p className="policy-para">In case you haven’t received any confirmation email from MyWicket11 after 
+        <p className="responsible-para">I did not get any confirmation email after I signed up.</p>
+        <p className="responsible-para">In case you haven’t received any confirmation email from MyWicket11 after 
         registering with MyWicket11 there could be 2 reasons:</p>
-        <p className="policy-para">The mail sent by MyWicket11 might have been marked as “Spam” and could
+        <p className="responsible-para">The mail sent by MyWicket11 might have been marked as “Spam” and could
          have been sent to the spams folder of your mailbox. In such case, please check the spam folder of 
          your mailbox and mark MyWicket11 as ‘Not Spam,’ additionally add MyWicket11 to your contacts in order
           to avoid any future inconvenience.</p>
       </div>
 
 
-      <div className="each-policy-container">
-        {/* <h3 className="policy-heading">Disclosure/Sharing:</h3> */}
-        <p className="policy-para">
+      <div className="each-responsible-container">
+        {/* <h3 className="responsible-heading">Disclosure/Sharing:</h3> */}
+        <p className="responsible-para">
         The user might have mistyped his/her email address while registering with MyWicket11. 
         In such case, please reach out to us via our Contact Us page.
 
 
         </p>
 
-        <p className="policy-para">I don’t know much about cricket – can I still play?</p>
-        <p className="policy-para">The online fantasy cricket gaming services offered by MyWicket11 are for everyone
+        <p className="responsible-para">I don’t know much about cricket – can I still play?</p>
+        <p className="responsible-para">The online fantasy cricket gaming services offered by MyWicket11 are for everyone
          including those who don’t know much about cricket. So, yes, you can play even if you don’t know much about 
          cricket. In addition, the “How to play” section of MyWicket11 website can assist you in playing the game by
           briefing you about the playing tactics.</p>
