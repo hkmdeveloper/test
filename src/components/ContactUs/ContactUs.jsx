3feea5c3046bc4ff93336c324d35aac1b91cd658
hkmdeveloper <developer@hkmedia.co.in>
2024-06-11 08:37:09 +0530

    Update ContactUs.jsx

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -3,12 +3,12 @@ import "./ContactUs.css";
 
 export const ContactUs = () => {
     return (
-      <div className="privacy-policy-container">
+      <div className="contactUs-container">
   
   <p className="policy-para">......</p>
-        <div className="each-policy-container">
-          <h3 className="policy-heading">Responsible Play</h3>
-          <p className="policy-para">
+        <div className="each-contactUs-container">
+          <h3 className="contactUs-heading">Responsible Play</h3>
+          <p className="contactUs-para">
           What is MyWicket11?
           </p>
           </div>
