b72e234e1672346aa3b076a9c366163e0d977ccd
hkmdeveloper <developer@hkmedia.co.in>
2024-06-11 08:33:08 +0530

    Update RefundPolicy.jsx

--- src/components/RefundPolicy/RefundPolicy.jsx
+++ src/components/RefundPolicy/RefundPolicy.jsx
@@ -3,18 +3,18 @@ import "./RefundPolicy.css";
 
 export const RefundPolicy = () => {
   return (
-    <div className="privacy-policy-container">
+    <div className="refund-policy-container">
 
-<p className="policy-para">......</p>
-      <div className="each-policy-container">
-        <h3 className="policy-heading">Refund Policy</h3>
+<p className="refund-refund-policy-para">......</p>
+      <div className="each-refund-policy-container">
+        <h3 className="refund-policy-heading">Refund Policy</h3>
         
 
-        <p className="policy-para">Any amount once added to the My Wicket11 account of user can't be 
+        <p className="refund-policy-para">Any amount once added to the My Wicket11 account of user can't be 
         refunded back to the user in any case, and the amount can be used to join the leagues, fun features 
         and Contest(s) organised by My Wicket11.</p>
 
-        <p className="policy-para"> The individuals using the services and Fun Features provided by My Wicket11 
+        <p className="refund-policy-para"> The individuals using the services and Fun Features provided by My Wicket11 
         are not permitted to cancel any transaction which has been once confirmed. However, My Wicket11 is 
         authorised to permit any user to cancel a transaction and refund the transaction amount paid by the 
         user at its sole and absolute discretion, in an event :If the User sends a written request to My 
@@ -24,7 +24,7 @@ export const RefundPolicy = () => {
           not be liable to refund any amount thereafter.</p>
 
 
-        <p className="policy-para">My Wicket11 reserves the rights to refund the transaction amount to the user 
+        <p className="refund-policy-para">My Wicket11 reserves the rights to refund the transaction amount to the user 
         in the above mentioned case at its sole and absolute discretion post deducting applicable
          cancellation charges and taxes. Further, amid the transaction process, the users should take 
          a note of the additional terms and conditions which significantly govern the transaction. In 
@@ -33,7 +33,7 @@ export const RefundPolicy = () => {
          transactions shall prevail. The refund is only applicable to the amount used to join any league and 
          shall be refunded to the My Wicket11 User Account source, as deducted.</p>
 
-        <p className="policy-para">In case of any unforeseen event, e.g. Force majeure, event cancellation,
+        <p className="refund-policy-para">In case of any unforeseen event, e.g. Force majeure, event cancellation,
         or technical errors and others, only league joining amount will be refunded as used to join the league, 
         there will be no claim on the winning as per the rank.</p>
 
